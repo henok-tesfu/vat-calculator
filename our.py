c2e8dfcc7b1ccc97b366565d1a65b04576dbda9d
henok-tesfu <henoktesf48@gmail.com>
2024-10-01 00:23:26 +0300

    added new files and added new commit

---
+++ our.py
@@ -0,0 +1,4 @@
+#  just created a python file to test the import statement
+#  and the module creation
+#  this file will be imported in the main.py file
+#  and the functions will be called from the main.py file
